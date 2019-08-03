b7e66d7488269a1e60cb15a04b771f717b20eaff
Ali Yucad <developer@horizonistanbul.com>
2019-08-03 15:06:56 +0300

    refactored for recycler view adapter

--- app/src/main/java/com/horizonistanbul/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/horizonistanbul/coderswag/Controller/MainActivity.kt
@@ -2,9 +2,11 @@ package com.horizonistanbul.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.horizonistanbul.coderswag.Adapters.CategoryAdapter
+import com.horizonistanbul.coderswag.Adapters.CategoryRecycleAdapter
 import com.horizonistanbul.coderswag.Model.Category
 import com.horizonistanbul.coderswag.R
 import com.horizonistanbul.coderswag.Services.DataService
@@ -12,15 +14,19 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryAdapter
+    lateinit var adapter : CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
 
     /*    categoryListView.setOnItemClickListener { adapterView, view, i, l ->
             val category = DataService.categories[i]
